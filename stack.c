c9c6b4f2ba935e4b0550b4b97537883bdfe8d14b
mahendra purohit <mahendrapurohit9889@gmail.com>
2023-10-09 19:59:23 +0530

    09/10/2023

---
+++ stack.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+#define n 5
+int a[n], top = -1;
+
+int intsertEnd(int data)
+{
+    if(top>= n-1){
+        printf("stack is overflow");
+
+    }else 
+    {
+        top++;
+        a[top]=data;
+    }
+}
+int delete (){
+    if(top<0){
+       
+       printf(" stack underflow");
+    
+    }else{
+    
+    
+      top--;
+
+    }
+    
+}
+
+int display(){
+    for(int i=0;i<=top; i++){
+
+        printf("%d ",a[i]);
+        
+    }
+    printf("\n");
+} 
+
+int main (){
+
+    int y;
+
+    printf("Press 1 to insert \n");
+    printf("Press 2 to delete \n");
+    printf("Press 3 to to display\n");
+    printf("Press 0 to exit\n");
+
+    scanf("%d",&y);
+    
+
+    switch(y){
+        
+        case 1:
+        intsertEnd(100);
+        intsertEnd(200);
+        intsertEnd(300);
+        intsertEnd(400);
+        intsertEnd(500);
+        break;
+
+        case 2:
+        delete();
+        break;
+
+        case 3:
+        display();
+        break;
+
+        case 0:
+        break;
+
+        dafualt:
+        break;
+
+    }
+
+    display();
+}
