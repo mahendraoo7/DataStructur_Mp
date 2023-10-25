a830be01d42eb9e5c264a97f1e0130548a37ae4b
mahendra purohit <mahendrapurohit9889@gmail.com>
2023-10-25 09:53:40 +0530

    25-10-2023

---
+++ simple_linklist2.c
@@ -0,0 +1,107 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+   int data;
+   struct node *next;
+};
+struct node *head = NULL;
+
+void insertEnd(int val)
+{
+    printf("enter val ");
+    scanf("%d",&val);
+    struct node *ptr = head;
+    struct node *temp = malloc(sizeof(struct node));
+    temp->data = val;
+    temp->next = NULL;
+
+    if(head == NULL){
+        head = temp;
+        return;
+    }
+    while(ptr->next != NULL)
+    {
+        ptr = ptr->next;
+    }
+    ptr->next = temp;
+    return;
+}
+void deleteEnd()
+{
+    struct node *ptr = head;
+    struct node *prev;
+    if(head == NULL)
+    {
+        printf("list is alredey empty....");
+
+    }else if(head->next == NULL)
+    {
+        head = NULL;
+        free(ptr);
+        return;
+    }
+    while(ptr->next != NULL)
+    {
+        prev =ptr;
+        ptr = ptr->next;
+    }
+    prev->next = NULL;
+    free(ptr);
+    return;
+}
+void display()
+{
+    struct node *ptr = head;
+    if(head == NULL)
+    {
+        printf("\n list is empty...");
+    }
+    else{
+        while (ptr != NULL)
+        {
+            printf("%d\t",ptr->data);
+            ptr = ptr -> next;
+        }    
+    }
+    printf("\n");
+}
+int main()
+{
+    int ch, data, val;
+    do
+    {
+        
+    
+        printf("Press 1 For insertEnd \n");
+        printf("Press 2 For DeleteEnd \n");
+        printf("Press 3 for Display \n");
+        printf("Press 0 for EXIT \n");
+
+        printf(" Enter Number \n");
+        scanf("%d", &ch);
+
+        switch (ch)
+        {
+        case 1:
+            insertEnd(val);
+            break;
+
+        case 2:
+            deleteEnd();
+            break;
+
+        case 3:
+            display();
+            break;
+
+        case 0:
+            printf("\n Eedxit \n");
+            break;
+
+        default:
+            printf("Try Again ");
+            break;
+        }
+    } while (ch != 0);
+    return 0;
+}
